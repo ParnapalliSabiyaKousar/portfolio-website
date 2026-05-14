e8b55d77b906c56d92797ee7848bc5bca6d7ec48
Sabiya Kousar <sabiyakousar999@gmail.com>
2026-05-14 16:46:10 +0530

    Fixed server

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,25 @@
-const mongoose = require('mongoose');
+const express = require('express');
+const cors = require('cors');
 
-mongoose.connect('YOUR_MONGODB_URL')
-.then(() => console.log('MongoDB Connected'))
-.catch(err => console.log(err));
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Portfolio Backend Running');
+});
+
+app.post('/contact', (req, res) => {
+    console.log(req.body);
+
+    res.status(200).json({
+        message: 'Message received successfully'
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
